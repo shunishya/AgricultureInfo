273209bd5ae0cc5fe54dd8c147e47a468d6e3bde
shunishya <shunishya.shrestha@gamil.com>
2014-11-24 09:12:28 +0545

    commit before merger

--- KrishiGhar/gen/com/handmark/pulltorefresh/library/R.java
+++ KrishiGhar/gen/com/handmark/pulltorefresh/library/R.java
@@ -49,23 +49,23 @@ public final class R {
 		public static final int indicator_bg_top = 0x7f02007a;
 	}
 	public static final class id {
-		public static final int both = 0x7f090003;
-		public static final int disabled = 0x7f090000;
+		public static final int both = 0x7f090009;
+		public static final int disabled = 0x7f09000a;
 		public static final int fl_inner = 0x7f090059;
-		public static final int flip = 0x7f090008;
-		public static final int gridview = 0x7f090009;
-		public static final int manualOnly = 0x7f090004;
-		public static final int pullDownFromTop = 0x7f090005;
-		public static final int pullFromEnd = 0x7f090002;
-		public static final int pullFromStart = 0x7f090001;
-		public static final int pullUpFromBottom = 0x7f090006;
+		public static final int flip = 0x7f090010;
+		public static final int gridview = 0x7f090000;
+		public static final int manualOnly = 0x7f09000b;
+		public static final int pullDownFromTop = 0x7f09000c;
+		public static final int pullFromEnd = 0x7f09000d;
+		public static final int pullFromStart = 0x7f09000e;
+		public static final int pullUpFromBottom = 0x7f09000f;
 		public static final int pull_to_refresh_image = 0x7f09005a;
 		public static final int pull_to_refresh_progress = 0x7f09005b;
 		public static final int pull_to_refresh_sub_text = 0x7f09005d;
 		public static final int pull_to_refresh_text = 0x7f09005c;
-		public static final int rotate = 0x7f090007;
-		public static final int scrollview = 0x7f09000b;
-		public static final int webview = 0x7f09000a;
+		public static final int rotate = 0x7f090011;
+		public static final int scrollview = 0x7f090002;
+		public static final int webview = 0x7f090001;
 	}
 	public static final class layout {
 		public static final int pull_to_refresh_header_horizontal = 0x7f030020;
